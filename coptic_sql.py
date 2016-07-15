1cd9c040ec059fb0e04187c6be04d38338d021e3
Shuo Zhang <zangsir@gmail.com>
2016-07-15 15:20:09 -0400

    new features

--- coptic_sql.py
+++ coptic_sql.py
@@ -18,24 +18,28 @@ def setup_db():
     cur = conn.cursor()
     # Drop tables if they exist
     cur.execute("DROP TABLE IF EXISTS coptic_docs")
+    cur.execute("DROP TABLE IF EXISTS users")
     conn.commit()
     
     # Create tables
+    cur.execute('''CREATE TABLE IF NOT EXISTS users
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, username text)''')
     cur.execute('''CREATE TABLE IF NOT EXISTS coptic_docs
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, status text, assigned text, filename text, content text)''')
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, status text,assignee_users_id INTEGER ,filename text, content text,FOREIGN KEY(assignee_users_id) REFERENCES users(id))''')
 
     
     conn.commit()
     conn.close()
     
 
-def create_document(name,status,assigned,filename,content):
+def create_document(name,status,assigned_id,filename,content):
     content=content.replace('"',"'")
     dbpath = os.path.dirname(os.path.realpath(__file__)) + os.sep +".."+os.sep+"coptic.db"
     conn = sqlite3.connect(dbpath)
     cur = conn.cursor()
-    cur.execute('''INSERT INTO {tn}(name,status,assigned,filename,content) VALUES({name},{status},{assigned},{filename},{content})'''.format(tn="coptic_docs",name='"'+name+'"',status='"'+status+'"',assigned='"'+assigned+'"',filename='"'+filename+'"',content='"'+content+'"'))
+    cur.execute('''INSERT INTO {tn}(name,status,assignee_users_id,filename,content) VALUES({name},{status},{assigned},{filename},{content})'''.format(tn="coptic_docs",name='"'+name+'"',status='"'+status+'"',assigned='"'+assigned_id+'"',filename='"'+filename+'"',content='"'+content+'"'))
     conn.commit()
+    conn.close()
 
 
 def generic_query(sql,params):
@@ -67,8 +71,21 @@ def update_assignee(id,assignee):
 def update_status(id,status):
     generic_query("UPDATE coptic_docs SET status=? WHERE id=?",(status,id))
 
+def update_docname(id,docname):
+    generic_query("UPDATE coptic_docs SET name=? WHERE id=?",(docname,id))
 
 
+def update_filename(id,filename):
+    generic_query("UPDATE coptic_docs SET filename=? WHERE id=?",(filename,id))
+
+
+def create_user(username):
+    dbpath = os.path.dirname(os.path.realpath(__file__)) + os.sep +".."+os.sep+"coptic.db"
+    conn = sqlite3.connect(dbpath)
+    cur = conn.cursor()
+    cur.execute('''INSERT INTO {tn}(username) VALUES({username})'''.format(tn="users",username='"'+username+'"'))
+    conn.commit()
+    conn.close()
 
 
 
--- editor.py
+++ editor.py
@@ -20,40 +20,53 @@ def perform_action(text_content):
 
 def load_page(theform):
     max_id=generic_query("SELECT MAX(id) AS max_id FROM coptic_docs","")[0][0]
+
     if theform.getvalue('newdoc'):
+        #when creating new document from landing page, a new id as max_id+1 is passed
         doc_id=theform.getvalue('id')
         perform_action(doc_id)
         doc_name="new document"
         file_name="document"
-        assignee="user"
+        assignee="1"
         status="new"
         text_content=""
         js="""<script> var docid = """ + str(doc_id)
         js+=""";localStorage.setItem("docid", docid);"""
         js+="""document.getElementById('zangsir').value = docid;</script>"""
 
-
+    
     elif theform.getvalue('id'):
-
+        #this should come from either creating new doc or 'editing doc' in landing page
         doc_id=theform.getvalue('id')
         perform_action(doc_id)
+        #creating new doc case, assign some default values
         if int(doc_id)>int(max_id):
             doc_name="new document"
             file_name="doc.xml"
             status="new"
-            assignee="user"
-            
+            assignee="1"
+        #after clicking edit in landing page, editing existing doc case, get the values from the db. pull the content from db to be displayed in the editor window.
         else:
+            if theform.getvalue('edit_docname'):
+                docname=theform.getvalue('edit_docname')
+                perform_action(docname)
+                update_docname(doc_id,docname)
+            if theform.getvalue('edit_filename'):
+                filename=theform.getvalue('edit_filename')
+                perform_action(filename)
+                update_filename(doc_id,filename)
             text_content = generic_query("SELECT content FROM coptic_docs WHERE id=?",doc_id)[0][0]
             doc_name=generic_query("SELECT name FROM coptic_docs WHERE id=?",doc_id)[0][0]
             file_name=generic_query("SELECT filename FROM coptic_docs WHERE id=?",doc_id)[0][0]
-            assignee=generic_query("SELECT assigned FROM coptic_docs WHERE id=?",doc_id)[0][0]
+            assignee=generic_query("SELECT username FROM coptic_docs JOIN USERS ON COPTIC_DOCS.ASSIGNEE_USERS_ID = USERS.ID WHERE coptic_docs.id=?",doc_id)[0][0]
             status=generic_query("SELECT status FROM coptic_docs WHERE id=?",doc_id)[0][0]
             
 
         js="""<script> var docid = """ + str(doc_id)
         js+=""";localStorage.setItem("docid", docid);"""
         js+="""document.getElementById('zangsir').value = docid;</script>"""
+    #in the case of reloading after hitting 'save', either create new doc into db, or update db
+    #codemirror sends the form with its code content in it before 'save' so we just display it again
     if theform.getvalue('code'):
         text_content = theform.getvalue('code')
         #max_id=generic_query("SELECT MAX(id) AS max_id FROM coptic_docs","")[0][0]
@@ -61,13 +74,32 @@ def load_page(theform):
             create_document(doc_name,status,assignee,file_name,text_content)
         else:
             save_changes(doc_id,text_content)
+    
+
+
+
+
+    edit_docname="in progress"
+    edit_filename="in progress"
+    edit_status="in progress"
+    edit_assignee="in progress"
+    
+    edit_docname = """<input type='text' name='edit_docname' value=''> <input type='submit' value='change'>"""
+
+    edit_filename = """<input type='text' name='edit_filename' value=''> <input type='submit' value='change'>"""
 
     page= "Content-type:text/html\r\n\r\n"
     page+= urllib.urlopen("editor_codemir.html").read()
     page=page.replace("**content**",text_content)
-    page=page.replace("**docname**",doc_name+"("+file_name+")")
+    page=page.replace("**docname**",doc_name)
+    page=page.replace("**filename**",file_name)
     page=page.replace("**assigned**",assignee)
     page=page.replace("**status**",status)
+    page=page.replace("**editdocname**",edit_docname)
+    page=page.replace("**editstatus**",edit_status)
+    page=page.replace("**editfilename**",edit_filename)
+    page=page.replace("**editassignee**",edit_assignee)
+
     page=page.replace("**js**",js)
 
     return page
